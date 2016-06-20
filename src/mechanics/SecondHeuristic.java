ddbb76391fed635d906ea9f0e61238bd5c06ae9f
Jorge Signol <jorge.signol@pucp.pe>
2016-06-19 23:37:05 -0500

    actualizacioon heur 2

--- src/mechanics/SecondHeuristic.java
+++ src/mechanics/SecondHeuristic.java
@@ -17,23 +17,70 @@ public class SecondHeuristic {
     
     int cantPoblacionMax = 26;
     double[] pesos;
-    public SecondHeuristic (){
+    public SecondHeuristic (List<Pokemon> playerTeam, List<Pokemon> enemyTeam){
         //Pesos manuales
         pesos[0] = 0.5;
         pesos[1] = 0.2;
         pesos[2] = 0.2;
         pesos[3] = 0.1;
+        
+        
+        
+        //Algoritmo genetico
+        
+        //Poblacion
+        
     }
-    public double calculate_factor_1(List<Pokemon> playerTeam, List<Pokemon> enemyTeam, double [] weights){
-        return 0;
+    
+    // FACTORES PARA CREAR ESTRATEGIA EN JUGADA DE LA PC
+    public double calculate_factor_1(List<Pokemon> playerTeam, List<Pokemon> enemyTeam){
+        //suponiendo que A es player (humano), B es enemy (PC)
+        int sumaVidaEnemy = 0, sumaVidaPlayer=0;
+        for (int i = 0; i<playerTeam.size();i++){
+            sumaVidaPlayer=sumaVidaPlayer+playerTeam.get(i).getHitPoints();
+        }
+        for (int i = 0; i<enemyTeam.size();i++){
+            sumaVidaEnemy=sumaVidaEnemy+enemyTeam.get(i).getHitPoints();
+        }        
+        return sumaVidaEnemy - sumaVidaPlayer;
     }
-    public double calculate_factor_2(List<Pokemon> playerTeam, List<Pokemon> enemyTeam, double [] weights){
-        return 0;
+    public double calculate_factor_2(List<Pokemon> playerTeam, List<Pokemon> enemyTeam){
+        int vivosEnemy = 0, vivosPlayer=0;
+        for (int i = 0; i<playerTeam.size();i++){
+            if(playerTeam.get(i).getHitPoints()>0)
+                vivosPlayer++;
+        }
+        for (int i = 0; i<enemyTeam.size();i++){
+            if(enemyTeam.get(i).getHitPoints()>0)
+                vivosEnemy++;
+        }       
+        return vivosEnemy - vivosPlayer;
     }
-    public double calculate_factor_3(List<Pokemon> playerTeam, List<Pokemon> enemyTeam, double [] weights){
-        return 0;
+    public double calculate_factor_3(List<Pokemon> playerTeam, List<Pokemon> enemyTeam){
+        int sumaAtaqueEnemy = 0, sumaAtaquePlayer=0;
+        for (int i = 0; i<playerTeam.size();i++){
+            sumaAtaquePlayer=sumaAtaquePlayer+playerTeam.get(i).getAttack();
+        }
+        for (int i = 0; i<enemyTeam.size();i++){
+            sumaAtaqueEnemy=sumaAtaqueEnemy+enemyTeam.get(i).getAttack();
+        }        
+        return sumaAtaqueEnemy - sumaAtaquePlayer;
     }
-    public double calculate_factor_4(List<Pokemon> playerTeam, List<Pokemon> enemyTeam, double [] weights){
-        return 0;
+    public double calculate_factor_4(List<Pokemon> playerTeam, List<Pokemon> enemyTeam){
+        int sumaDefensaEnemy = 0, sumaDefensaPlayer=0;
+        for (int i = 0; i<playerTeam.size();i++){
+            sumaDefensaPlayer=sumaDefensaPlayer+playerTeam.get(i).getAttack();
+        }
+        for (int i = 0; i<enemyTeam.size();i++){
+            sumaDefensaEnemy=sumaDefensaEnemy+enemyTeam.get(i).getAttack();
+        }        
+        return sumaDefensaEnemy - sumaDefensaPlayer;
     }
+    
+    //FUNCION DE CREACION DE POBLACION
+    public void crear_poblacion(){
+        
+    }
+    
+    //FUNCION DE FITNESS
 }
