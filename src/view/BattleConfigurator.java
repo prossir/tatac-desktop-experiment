1b81b76162c584b6f42c6c28b57bb1e1413c047c
Paolo <diagonal_zero@hotmail.com>
2016-06-21 00:58:09 -0500

    barcenamon included new screens

--- src/view/BattleConfigurator.java
+++ src/view/BattleConfigurator.java
@@ -34,37 +34,69 @@ public class BattleConfigurator extends javax.swing.JFrame {
 
         b_training = new javax.swing.JButton();
         b_battle = new javax.swing.JButton();
+        b_trained_minimax = new javax.swing.JButton();
+        b_barcenamon_battle = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         b_training.setText("Train Computer");
 
-        b_battle.setText("Battle");
+        b_battle.setText("Minimax");
         b_battle.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 b_battleActionPerformed(evt);
             }
         });
 
+        b_trained_minimax.setText("Trained Minimax");
+        b_trained_minimax.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                b_trained_minimaxActionPerformed(evt);
+            }
+        });
+
+        b_barcenamon_battle.setText("Random");
+        b_barcenamon_battle.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                b_barcenamon_battleActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Pokemon Battle Simulator");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(38, 38, 38)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(b_battle, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(b_training, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(405, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(81, 81, 81)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(94, 94, 94)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(b_trained_minimax, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(b_training, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(b_battle, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(b_barcenamon_battle, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(92, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(121, 121, 121)
-                .addComponent(b_training)
-                .addGap(48, 48, 48)
-                .addComponent(b_battle)
-                .addContainerGap(265, Short.MAX_VALUE))
+                .addGap(37, 37, 37)
+                .addComponent(jLabel1)
+                .addGap(34, 34, 34)
+                .addComponent(b_training, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(b_trained_minimax, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(b_battle, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(b_barcenamon_battle, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(51, Short.MAX_VALUE))
         );
 
         pack();
@@ -81,6 +113,14 @@ public class BattleConfigurator extends javax.swing.JFrame {
         BattleInterface battleInterface = new BattleInterface(playerTeam, computerTeam);
         battleInterface.setVisible(true);
     }//GEN-LAST:event_b_battleActionPerformed
+
+    private void b_trained_minimaxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b_trained_minimaxActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_b_trained_minimaxActionPerformed
+
+    private void b_barcenamon_battleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b_barcenamon_battleActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_b_barcenamon_battleActionPerformed
 
     /**
      * @param args the command line arguments
@@ -112,7 +152,10 @@ public class BattleConfigurator extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton b_barcenamon_battle;
     private javax.swing.JButton b_battle;
+    private javax.swing.JButton b_trained_minimax;
     private javax.swing.JButton b_training;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
